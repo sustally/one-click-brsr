0a05e4c36c85009461070ab5b3860e659e65fc3b
mukul <Mukulgupta1712@gmail.com>
2024-12-05 19:45:55 +0530

    create API for log in super admin

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,10 @@ const app = express();
 
 //------------------------- Super Admin Routes -----------------------
 const createSuperAdminRoute = require("./superAdminRoutes/createSuperAdminRoute/createSuperAdmin.route");
+const loginSuperAdminRoute = require("./superAdminRoutes/loginSuperAdminRoute/loginSuperAdmin.route");
 
 //------------------------- Super Admin Routes -----------------------
 app.use("/superAdmin", createSuperAdminRoute);
+app.use("/superAdmin", loginSuperAdminRoute);
+
 module.exports = app;
