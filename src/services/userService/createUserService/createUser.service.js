4a1a5976f2941dbe55f0226c830c6dbf6e21760a
aman-gupta <aman@sustally.com>
2024-12-26 14:50:57 +0530

    remove default BRSRreport request body in create user API

--- src/services/userService/createUserService/createUser.service.js
+++ src/services/userService/createUserService/createUser.service.js
@@ -3,7 +3,6 @@ const ApiError = require("../../../utils/ApiError");
 const User = require("../../../models/userModel/user.model");
 const SuperAdmin = require("../../../models/superAdminModel/superAdmin.model");
 const logger = require("../../../config/logger");
-const brsrReportBody = require("../../../utils/brsrReportBody");
 const {
   createBrsrReport,
 } = require("../../../services/brsrReportService/createBrsrReportService/createBrsrReport.service");
@@ -39,7 +38,7 @@ const createUser = async (body, userId, userType) => {
       throw new ApiError(httpStatus.UNAUTHORIZED, "Something went wrong");
     }
     user.password = undefined;
-    const brsrReport = await createBrsrReport(brsrReportBody, user.userId);
+    const brsrReport = await createBrsrReport(user.userId);
     return user;
   } catch (error) {
     logger.error(
