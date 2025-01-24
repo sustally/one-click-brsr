a2f2257c8abe10fa1b5d5169a77e9c51d8c32d25
aman-gupta <aman@sustally.com>
2025-01-24 19:07:45 +0530

    add logic for generate BRSR Report in NSE Format

--- src/services/brsrReportService/createXmlFileService/createXmlFile.service.js
+++ src/services/brsrReportService/createXmlFileService/createXmlFile.service.js
@@ -9,6 +9,33 @@ const js2xmlparser = require("js2xmlparser");
 const path = require("path");
 const uuid = require("uuid");
 
+const addContextRefToTags = (data) => {
+  if (typeof data === "object" && data !== null) {
+    for (const key in data) {
+      if (!key.startsWith("in-capmkt:") && key !== "@") {
+        // Preserve the value and add the namespace and attributes
+        const value = data[key];
+        const newKey = `in-capmkt:${key}`;
+
+        if (typeof value === "object") {
+          // If the value is an object, recursively apply the function
+          data[newKey] = addContextRefToTags(value);
+        } else {
+          // Add the contextRef attribute and preserve the value as a string
+          data[newKey] = {
+            "@": { contextRef: "DCYMain" }, // Attribute
+            value: value, // Text value
+          };
+        }
+        delete data[key]; // Remove the old key
+      } else if (typeof data[key] === "object") {
+        addContextRefToTags(data[key]); // Recursively process nested objects
+      }
+    }
+  }
+  return data;
+};
+
 const createXmlFile = async (userId, brsrReportId) => {
   try {
     logger.info("update BRSR report API called");
@@ -100,8 +127,39 @@ const createXmlFile = async (userId, brsrReportId) => {
     delete sanitizedData.createdAt;
     delete sanitizedData.updatedAt;
 
-    // Convert BRSR report to XML
-    const xmlContent = js2xmlparser.parse("BRSRReport", sanitizedData);
+    addContextRefToTags(sanitizedData);
+
+    const namespaces = {
+      "@": {
+        "xmlns:xbrli": "http://www.xbrl.org/2003/instance",
+        "xmlns:Non-SI": "http://www.xbrl.org/2009/utr",
+        "xmlns:dtr-types": "http://www.xbrl.org/dtr/type/CR/2023-12-20",
+        "xmlns:in-capmkt": "https://www.sebi.gov.in/xbrl/2024-04-30/in-capmkt",
+        "xmlns:in-capmkt-ent":
+          "https://www.sebi.gov.in/xbrl/BRSR/2024-04-30/in-capmkt/in-capmkt-ent",
+        "xmlns:in-capmkt-roles":
+          "https://www.sebi.gov.in/xbrl/BRSR/2024-04-30/in-capmkt-roles",
+        "xmlns:in-capmkt-types":
+          "https://www.sebi.gov.in/xbrl/2024-04-30/in-capmkt-types",
+        "xmlns:iso4217": "http://www.xbrl.org/2003/iso4217",
+        "xmlns:link": "http://www.xbrl.org/2003/linkbase",
+        "xmlns:negated": "http://www.xbrl.org/2009/role/negated",
+        "xmlns:net": "http://www.xbrl.org/2009/role/net",
+        "xmlns:nonnum": "http://www.xbrl.org/dtr/type/non-numeric",
+        "xmlns:num": "http://www.xbrl.org/dtr/type/numeric",
+        "xmlns:xbrldi": "http://xbrl.org/2006/xbrldi",
+        "xmlns:xbrldt": "http://xbrl.org/2005/xbrldt",
+        "xmlns:xl": "http://www.xbrl.org/2003/XLink",
+        "xmlns:xlink": "http://www.w3.org/1999/xlink",
+      },
+      ...sanitizedData,
+    };
+
+    const xmlContent = js2xmlparser.parse("BRSRReport", namespaces, {
+      declaration: { include: true },
+      format: { pretty: true },
+    });
+
     if (!xmlContent) {
       logger.error("Something went wrong when creating XML");
       throw new ApiError(
